1065ea30017b6ba9b67d9d8791e50bd8b8b7bcf9
Razvan Rauta <rrauta@klika-tech.com>
2021-11-08 16:12:19 +0300

    Fixed deprecations

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
-import 'auth_failure.dart';
 
+import 'auth_failure.dart';
 import 'value_objects.dart';
 
 abstract class IAuthFacade {
--- lib/presentation/sign_in/widgets/sign_in_form.dart
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: file_names
-
 import '../../../application/auth/sign_in_form/bloc/sign_in_form_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
